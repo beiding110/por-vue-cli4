3272e6d826a7f89ac9eb1a81faeb896474d38cb5
yzh <beiding110@gmail.com>
2021-04-12 14:20:59 +0800

    【修改】兼容ie内出现的hashchange无响应问题；

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+    name: "App",
+    mounted() {
+        if (!!window.ActiveXObject || 'ActiveXObject' in window) {
+            window.addEventListener('hashchange', () => {
+                let currentPath = window.location.hash.slice(1);
+                if (this.$route.path !== currentPath) {
+                    this.$router.push(currentPath);
+                }
+            }, false);
+        }
+    },
+};
 </script>
 
 <style>
-
 </style>
