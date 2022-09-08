95f68103c96d75e5f4815c9a0c1aca480ed324ad
yzh <beiding110@gmail.com>
2022-09-08 15:13:53 +0800

    【修改】checkbox格式化，功能增强；

--- src/components/my/checkbox.vue
+++ src/components/my/checkbox.vue
@@ -1,22 +1,31 @@
 <template>
     <span class="my-checkbox">
-        <span v-if="!inAttr(readonly)">
+        <template v-if="!readonly">
             <el-checkbox-group v-model="model" style="display:inline-block;">
-                <el-checkbox v-for="item in list" 
-                    :key="item[props.value]" 
-                    :label="item[props.value]"
+                <component 
+                :is="`el-checkbox${button ? '-button' : ''}`"
+                v-for="item in list" 
+                :key="item[props.value]" 
+                :label="item[props.value]"
                 >
                     {{item[props.label]}}
-                </el-checkbox>
+                </component>
             </el-checkbox-group>
 
-            <el-checkbox v-model="otherController" style="margin-left:1em;" v-if="other" disabled>
-                <el-input v-model="input" placeholder="其它" size="mini"></el-input>
+            <el-checkbox 
+            v-if="other" 
+            v-model="otherController" 
+            style="margin-left:1em;" 
+            disabled
+            >
+                <el-input v-model="inputModel" placeholder="其它" size="mini"></el-input>
             </el-checkbox>
-        </span>
-        <span v-else>
+        </template>
+
+        <template v-else>
             {{readonlyStr}}
-        </span>
+        </template>
+
         <slot></slot>
     </span>
 </template>
@@ -29,8 +38,8 @@ export default {
         value: {
             type: [Array, String],
             default() {
-                return []
-            }
+                return [];
+            },
         },
         // 自动请求的接口地址
         url: {
@@ -46,8 +55,8 @@ export default {
         data: {
             type: Array,
             default() {
-                return []
-            }
+                return [];
+            },
         },
         // 配置选项
         props: {
@@ -56,57 +65,63 @@ export default {
                 return {
                     value: 'key',
                     label: 'value'
-                }
-            }
+                };
+            },
         },
         // 只读
         readonly: {
             type: Boolean,
-            default: false
+            default: false,
         },
         // 全部选中
         all: {
             type: Boolean,
-            default: false
+            default: false,
         },
         // 绑定字符串
         modelStr: {
             type: Boolean,
-            default: false
+            default: false,
         },
         // 绑定字符串时，字符串间链接字符
         strSpliter: {
-            tyep: String,
-            default: ','
-        }
+            type: String,
+            default: ',',
+        },
+        button: {
+            type: Boolean,
+            default: false,
+        },
     },
     data: function () {
         return {
             list: [],
             otherController: false,
 
-            readonlyStr: ''
-        }
+            readonlyStr: '',
+        };
     },
     computed: {
         model: {
             get: function () {
-                if(this.modelStr) {
+                if (this.modelStr) {
                     var valueArr = [];
-                    if(getType(this.value) === 'string') {
+
+                    if (getType(this.value) === 'string') {
                         valueArr = this.value.split(this.strSpliter);
-                    };
-                    if(valueArr[0] === '') {
+                    }
+
+                    if (valueArr[0] === '') {
                         return valueArr.slice(1);
-                    } else {
-                        return valueArr;
-                    };
-                } else {
-                    return this.other ? this.value.slice(0, -1) : this.value;
-                };
+                    }
+                    
+                    return valueArr;
+                }
+                
+                return this.other ? this.value.slice(0, -1) : this.value;
             },
             set: function (val) {
-                if(this.modelStr) {
+                if (this.modelStr) {
                     this.$emit('input', val.join(this.strSpliter));
                     
                     this.$nextTick(() => {
@@ -115,63 +130,65 @@ export default {
                 } else {
                     if (this.other) {
                         var res = clone(val);
+
                         res.push(this.input);
-                    };
+                    }
+
                     this.$emit('input', this.other ? res : val);
                     
                     this.$nextTick(() => {
                         this.$emit('change', val.join(this.strSpliter));
                     });
-                };
+                }
             }
         },
-        input: {
+        inputModel: {
             get: function () {
-                return this.other ? this.value.slice(-1) : '';
+                return this.other ? this.model.slice(-1) : '';
             },
-            set: function (n) {
-                this.otherController = !!n;
+            set: function (val) {
+                this.otherController = !!val;
 
-                if (this.other) {
-                    var res = clone(this.model);
-                    res.push(n);
-                };
-                this.$emit('input', this.other ? res : val);
+                var res = clone(this.model);
+
+                res.push(val);
+
+                this.$emit('input', res);
                 
                 this.$nextTick(() => {
-                    this.$emit('change', val.join(this.strSpliter));
+                    this.$emit('change', res.join(this.strSpliter));
                 });
-            }
+            },
         },
     },
     watch: {
         data: {
             handler: function (n, o) {
-                if (n != o) {
+                if (n !== o) {
                     this.list = n;
                     this.readonlyStr = this.fetchKey();
-                };
+                }
             },
             deep: true
         },
-        value: function (n, o) {
+        value: function () {
             this.readonlyStr = this.fetchKey();
         },
-        input: function (n, o) {
-            if (n.slice(-1) != o.slice(-1) && /,|\/|;|、|，/.test(n.slice(-1))) {
-                ShowMsg.call(this, '无效的特殊字符', 'error');
+        inputModel: function (n, o) {
+            if (n.slice(-1) !== o.slice(-1) && /,|\/|;|、|，/.test(n.slice(-1))) {
+                showMsg.call(this, '无效的特殊字符', 'error');
                 this.input = o;
             }
-        }
+        },
     },
     methods: {
         queryData: function () {
-            if (!!this.url) {
-                this.$get(this.url, function (data) {
+            if (this.url) {
+                this.$get(this.url, (data) => {
                     this.list = data;
 
                     this.readonlyStr = this.fetchKey();
-                })
+                });
             } else {
                 this.list = this.data;
 
@@ -179,25 +196,22 @@ export default {
             }
         },
         fetchKey: function () {
-            var that = this;
             var res = [];
-            this.list.forEach(function (item) {
-                if (that.all) {
-                    res.push(item[that.props.label]);
-                } else {
-                    if (item[that.props.value] == that.value) {
-                        res.push(item[that.props.label]);
-                    }
-                }
 
-            })
+            this.list.forEach((item) => {
+                if (this.all) {
+                    res.push(item[this.props.label]);
+                } else if (item[this.props.value] === this.value) {
+                    res.push(item[this.props.label]);
+                }
+            });
             return res.join(',');
         }
     },
     mounted: function () {
         this.queryData();
     }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
