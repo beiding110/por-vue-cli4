e5aeca41b105f226eb7debb962a80e6178071e43
杨子豪 <beiding110@gmail.com>
2024-08-28 09:25:57 +0800

    【修改】优化upload-s文件预览标题展示效果；

--- src/components/my/upload-s/components/file-preivew-item.vue
+++ src/components/my/upload-s/components/file-preivew-item.vue
@@ -259,6 +259,12 @@ export default {
 
     .file-preview-dialog {
         ::v-deep {
+            .el-dialog__header {
+                text-overflow: ellipsis;
+                overflow: hidden;
+                white-space: nowrap;
+            }
+            
             .el-dialog__body{
                 text-align: center;
             }
